4c50086a8cceb6dc06161a3d67946d1bbde6e436
Hou Boyao <shangrila-1100@hotmail.com>
2025-02-23 16:39:52 +0800

    add a simple argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 pub mod loader;
 
+// use clap to handle command line arguments
+use clap::{arg, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Cmd {
+    #[arg(short, long)]
+    classpath: String,
+    class: String,
+    args: Vec<String>,
+}
+
 fn main() {
     println!("Hello, world!");
-    loader::load("./test.class".to_string());
+    // loader::load("./test.class".to_string());
+    let cmd = Cmd::parse();
+    println!("{:?}", cmd);
 }
