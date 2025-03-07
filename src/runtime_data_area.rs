37c52e2b7070da5f46146d5492e7ba14f804b582
Bob User <shangrila-1100@hotmail.com>
2025-03-07 17:38:28 +0800

    feat(runtime_data_area): expose methods in LocalVars and OperandStack for public access

--- src/runtime_data_area.rs
+++ src/runtime_data_area.rs
@@ -16,57 +16,57 @@ pub struct LocalVars {
 }
 
 impl LocalVars {
-    fn new(max_locals: usize) -> Self {
+    pub fn new(max_locals: usize) -> Self {
         Self {
             slots: vec![Slot { num: 0, objref: None }; max_locals],
         }
     }
 
-    fn set_int(&mut self, index: usize, val: i32) {
+    pub fn set_int(&mut self, index: usize, val: i32) {
         self.slots[index].num = val;
     }
 
-    fn get_int(&self, index: usize) -> i32 {
+    pub fn get_int(&self, index: usize) -> i32 {
         self.slots[index].num
     }
 
-    fn set_ref(&mut self, index: usize, obj: Rc<RefCell<Object>>) {
-        self.slots[index].objref = Some(obj);
+    pub fn set_ref(&mut self, index: usize, obj: Option<Rc<RefCell<Object>>>) {
+        self.slots[index].objref = obj;
     }
 
-    fn get_ref(&self, index: usize) -> Option<Rc<RefCell<Object>>> {
+    pub fn get_ref(&self, index: usize) -> Option<Rc<RefCell<Object>>> {
         match &self.slots[index].objref {
             Some(obj) => Some(obj.clone()),
             None => None,
         }
     }
 
-    fn get_float(&self, index: usize) -> f32 {
+    pub fn get_float(&self, index: usize) -> f32 {
         f32::from_bits(self.slots[index].num as u32)
     }
 
-    fn set_float(&mut self, index: usize, val: f32) {
+    pub fn set_float(&mut self, index: usize, val: f32) {
         self.slots[index].num = val.to_bits() as i32;
     }
 
-    fn get_long(&self, index: usize) -> i64 {
+    pub fn get_long(&self, index: usize) -> i64 {
         let low = self.slots[index].num as u32;
         let high = self.slots[index + 1].num as u32;
         ((high as i64) << 32) | (low as i64)
     }
 
-    fn set_long(&mut self, index: usize, val: i64) {
+    pub fn set_long(&mut self, index: usize, val: i64) {
         let low = val as u32;
         let high = (val >> 32) as u32;
         self.slots[index].num = low as i32;
         self.slots[index + 1].num = high as i32;
     }
 
-    fn get_double(&self, index: usize) -> f64 {
+    pub fn get_double(&self, index: usize) -> f64 {
         f64::from_bits(self.get_long(index) as u64)
     }
 
-    fn set_double(&mut self, index: usize, val: f64) {
+    pub fn set_double(&mut self, index: usize, val: f64) {
         self.set_long(index, val.to_bits() as i64);
     }
 }
@@ -77,29 +77,29 @@ pub struct OperandStack {
 }
 
 impl OperandStack {
-    fn new(max_stack: usize) -> Self {
+    pub fn new(max_stack: usize) -> Self {
         Self {
             size: 0,
             slots: vec![Slot { num: 0, objref: None }; max_stack],
         }
     }
 
-    fn push_int(&mut self, val: i32) {
+    pub fn push_int(&mut self, val: i32) {
         self.slots[self.size].num = val;
         self.size += 1;
     }
 
-    fn pop_int(&mut self) -> i32 {
+    pub fn pop_int(&mut self) -> i32 {
         self.size -= 1;
         self.slots[self.size].num
     }
 
-    fn push_ref(&mut self, obj: Rc<RefCell<Object>>) {
-        self.slots[self.size].objref = Some(obj);
+    pub fn push_ref(&mut self, obj: Option<Rc<RefCell<Object>>>) {
+        self.slots[self.size].objref = obj;
         self.size += 1;
     }
 
-    fn pop_ref(&mut self) -> Option<Rc<RefCell<Object>>> {
+    pub fn pop_ref(&mut self) -> Option<Rc<RefCell<Object>>> {
         self.size -= 1;
         match &self.slots[self.size].objref {
             Some(obj) => Some(obj.clone()),
@@ -107,17 +107,17 @@ impl OperandStack {
         }
     }
 
-    fn push_float(&mut self, val: f32) {
+    pub fn push_float(&mut self, val: f32) {
         self.slots[self.size].num = val.to_bits() as i32;
         self.size += 1;
     }
 
-    fn pop_float(&mut self) -> f32 {
+    pub fn pop_float(&mut self) -> f32 {
         self.size -= 1;
         f32::from_bits(self.slots[self.size].num as u32)
     }
 
-    fn push_long(&mut self, val: i64) {
+    pub fn push_long(&mut self, val: i64) {
         let low = val as u32;
         let high = (val >> 32) as u32;
         self.slots[self.size].num = low as i32;
@@ -125,34 +125,44 @@ impl OperandStack {
         self.size += 2;
     }
 
-    fn pop_long(&mut self) -> i64 {
+    pub fn pop_long(&mut self) -> i64 {
         self.size -= 2;
         let low = self.slots[self.size].num as u32;
         let high = self.slots[self.size + 1].num as u32;
         ((high as i64) << 32) | (low as i64)
     }
 
-    fn push_double(&mut self, val: f64) {
+    pub fn push_double(&mut self, val: f64) {
         self.push_long(val.to_bits() as i64);
     }
 
-    fn pop_double(&mut self) -> f64 {
+    pub fn pop_double(&mut self) -> f64 {
         f64::from_bits(self.pop_long() as u64)
+    }
+
+    pub fn push_slot(&mut self, slot: Slot) {
+        self.slots[self.size] = slot;
+        self.size += 1;
+    }
+
+    pub fn pop_slot(&mut self) -> Slot {
+        self.size -= 1;
+        self.slots[self.size].clone()
     }
 }
 
 pub struct Frame {
-    lower: Option<Box<Frame>>,
-    local_vars: Vec<i32>,
-    operand_stack: Stack,
+    pub lower: Option<Box<Frame>>,
+    pub local_vars: LocalVars,
+    pub operand_stack: OperandStack,
 }
 
 impl Frame {
     fn new(max_locals: usize, max_stack: usize) -> Self {
         Self {
             lower: None,
-            local_vars: vec![0; max_locals],
-            operand_stack: Stack::new(max_stack),
+            local_vars: LocalVars::new(max_locals),
+            operand_stack: OperandStack::new(max_stack),
         }
     }
 }
